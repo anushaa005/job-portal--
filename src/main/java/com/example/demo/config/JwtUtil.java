923a323f82b0c0beb76ca5f5146adb7908a68a47
Pushpendra Singh <pushpendra.singh@esoftech.com>
2025-12-31 13:08:47 +0530

    issues

--- src/main/java/com/example/demo/config/JwtUtil.java
+++ src/main/java/com/example/demo/config/JwtUtil.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
@@ -14,12 +15,11 @@ import java.util.Date;
 @Component
 public class JwtUtil {
 
+    @Value("${jwt.secret}")
+    private  String SECRET_KEY;
 
-    private static final String SECRET_KEY =
-            "your-very-secure-random-base64-encoded-secret-key-minimum-256-bits-long-for-hs256-algorithm";
-
-    private static final long EXPIRATION_TIME =
-            1000 * 60 * 60 * 24; // 24 hours
+    @Value("${jwt.expiration}")
+    private long EXPIRATION_TIME; // 24 hours
 
     public String generateToken(User user) {
 
